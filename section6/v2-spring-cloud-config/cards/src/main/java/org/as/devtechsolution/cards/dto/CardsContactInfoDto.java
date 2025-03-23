be25d94c2341d749beda00fad3613ff84a914c28
devtechsolution <46072271+devtechsolution@users.noreply.github.com>
2025-03-23 08:40:57 +0530

    Refresh Actuator path

--- section6/v2-spring-cloud-config/cards/src/main/java/org/as/devtechsolution/cards/dto/CardsContactInfoDto.java
+++ section6/v2-spring-cloud-config/cards/src/main/java/org/as/devtechsolution/cards/dto/CardsContactInfoDto.java
@@ -1,5 +1,7 @@
 package org.as.devtechsolution.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -10,6 +12,13 @@ import java.util.Map;
  */
 
 
+
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
