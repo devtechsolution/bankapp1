c3438335993851092ea8ed624853fba7dbd21025
devtechsolution <46072271+devtechsolution@users.noreply.github.com>
2025-03-30 23:38:19 +0530

    INITIAL COMMIT

---
+++ section10/accounts/src/main/java/org/as/devtechsolution/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,30 @@
+package org.as.devtechsolution.accounts.service.client;
+
+import jakarta.validation.constraints.Pattern;
+import org.as.devtechsolution.accounts.dto.CardsDto;
+import org.as.devtechsolution.accounts.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Aditya Srivastva
+ */
+
+@FeignClient("loans")
+public interface LoansFeignClient {
+
+
+    /**
+     * Fetch card details based on a mobile number.
+     *
+     * @param mobileNumber the mobile number used to fetch card details
+     * @return a ResponseEntity containing the card details wrapped in a CardsDto
+     */
+    @GetMapping(value = "/api/loan", consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestHeader("bankapp-correlation-id")
+                                                         String correlationId, @RequestParam String mobileNumber);
+
+}
---
+++ section10/accounts/src/main/java/org/as/devtechsolution/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,62 @@
+package org.as.devtechsolution.accounts.service.impl;
+
+import lombok.AllArgsConstructor;
+import org.as.devtechsolution.accounts.dto.AccountsDto;
+import org.as.devtechsolution.accounts.dto.CardsDto;
+import org.as.devtechsolution.accounts.dto.CustomerDetailsDto;
+import org.as.devtechsolution.accounts.dto.LoansDto;
+import org.as.devtechsolution.accounts.entity.Accounts;
+import org.as.devtechsolution.accounts.entity.Customer;
+import org.as.devtechsolution.accounts.exception.ResourceNotFoundException;
+import org.as.devtechsolution.accounts.mapper.AccountMapper;
+import org.as.devtechsolution.accounts.mapper.CustomerMapper;
+import org.as.devtechsolution.accounts.repository.AccountsRepository;
+import org.as.devtechsolution.accounts.repository.CustomerRepository;
+import org.as.devtechsolution.accounts.service.ICustomersService;
+import org.as.devtechsolution.accounts.service.client.CardsFeignClient;
+import org.as.devtechsolution.accounts.service.client.LoansFeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Aditya Srivastva
+ */
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+
+    /**
+     * Fetch customer details based on a given mobile number.
+     *
+     * @param mobileNumber Mobile number of the customer
+     * @return Customer details
+     */
+
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId,mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+    }
+}
