bd04dd631368de11201f4f76d444222929f37f49
devtechsolution <46072271+devtechsolution@users.noreply.github.com>
2025-03-31 00:01:12 +0530

    Circuit breaker implemetation with fallback controller in gatewayserver

--- section10/gatewayserver/src/main/java/org/as/devtechsolution/gatewayserver/GatewayserverApplication.java
+++ section10/gatewayserver/src/main/java/org/as/devtechsolution/gatewayserver/GatewayserverApplication.java
@@ -21,7 +21,9 @@ public class GatewayserverApplication {
                 .route(p -> p
                         .path("/bankapp/accounts/**")
                         .filters( f -> f.rewritePath("/bankapp/accounts/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
                         .path("/bankapp/loans/**")
