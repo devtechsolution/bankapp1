4d3f7d31c516f4fc1fcd0f8d8a6c250ae1797fca
devtechsolution <46072271+devtechsolution@users.noreply.github.com>
2025-03-31 07:54:57 +0530

    Implementation of retry mechanism for loan microservices in api gatewayserver. Limitation of it there is no fallback mechanism

--- section10/gatewayserver/src/main/java/org/as/devtechsolution/gatewayserver/GatewayserverApplication.java
+++ section10/gatewayserver/src/main/java/org/as/devtechsolution/gatewayserver/GatewayserverApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -23,12 +25,19 @@ public class GatewayserverApplication {
                         .filters( f -> f.rewritePath("/bankapp/accounts/(?<segment>.*)","/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
                                 .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
-                                        .setFallbackUri("forward:/contactSupport")))
+                                        .setFallbackUri("forward:/contactSupport"))
+//                                .retry(config -> config.setRetries(3)
+//                                        .setMethods(HttpMethod.GET)
+//                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000),2,true))
+                                        )
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
                         .path("/bankapp/loans/**")
                         .filters( f -> f.rewritePath("/bankapp/loans/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .retry(config -> config.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000),2,true)))
                         .uri("lb://LOANS"))
                 .route(p -> p
                         .path("/bankapp/cards/**")
--- section10/loans/src/main/java/org/as/devtechsolution/loans/controller/LoansController.java
+++ section10/loans/src/main/java/org/as/devtechsolution/loans/controller/LoansController.java
@@ -260,6 +260,7 @@ public class LoansController {
     )
     @GetMapping("/contact-info")
     public ResponseEntity<LoansContactInfoDto> getContactInfo() {
+        logger.debug("Invoke loan ContactInfo method");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(loansContactInfoDto);
